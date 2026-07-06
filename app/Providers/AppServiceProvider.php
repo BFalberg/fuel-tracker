a6408c4fde9ebf6c361f149d06cfa4cea1a3422c
BFalberg <benjaminfalberg@gmail.com>
2026-07-06 17:25:38 +0200

    setup to avoid clearing db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Console\Migrations\FreshCommand;
+use Illuminate\Database\Console\Migrations\ResetCommand;
+use Illuminate\Database\Console\Migrations\RollbackCommand;
+use Illuminate\Database\Console\Seeds\SeedCommand;
+use Illuminate\Database\Console\WipeCommand;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // This project is wired to the production database.
+        FreshCommand::prohibit();
+        ResetCommand::prohibit();
+        RollbackCommand::prohibit();
+        WipeCommand::prohibit();
+        SeedCommand::prohibit();
     }
 }
